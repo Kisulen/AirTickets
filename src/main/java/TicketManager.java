8d850bc01599812daf185e93671f591e9e69ab65
Maria Ustyuzhanina <elkaran@gmail.com>
2023-01-09 16:30:07 +0300

    Fast updated

--- src/main/java/TicketManager.java
+++ src/main/java/TicketManager.java
@@ -4,14 +4,6 @@ import java.util.Comparator;
 public class TicketManager {
 
     private TicketRepository repo;
-    Ticket ticket1 = new Ticket(1, 5357, "SVO", "CEK", 95);
-    Ticket ticket2 = new Ticket(2, 5597, "SVO", "CEK", 87);
-    Ticket ticket3 = new Ticket(3, 5260, "SVO", "CEK", 110);
-    Ticket ticket4 = new Ticket(4, 5480, "SVO", "CEK", 98);
-    Ticket ticket5 = new Ticket(5, 5386, "SVO", "CEK", 91);
-    Ticket ticket6 = new Ticket(6, 4690, "VKO", "CEK", 80);
-    Ticket ticket7 = new Ticket(7, 38500, "SVO", "JFK", 540);
-    Ticket ticket8 = new Ticket(8, 1500, "DME", "LED", 63);
 
     public TicketManager(TicketRepository repo) {
         this.repo = repo;
